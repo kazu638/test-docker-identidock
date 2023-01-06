e7004b41920664e8b7342bd3c6e40ce7843b0305
kazu638 <k89ut3903@gmail.com>
2023-01-07 02:21:18 +0900

    add test,jenkins

--- cmd.sh
+++ cmd.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
-echo ${ENV}
 
 if [ ${ENV} = 'DEV' ]; then
     echo "Running Development Server"
     exec python "identidock.py"
+elif [ ${ENV} = 'UNIT' ]; then
+    echo "Running Unit Tests"
+    exec python "tests.py"
 else
     echo "Running Production Server"
     exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
